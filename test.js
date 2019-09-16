4864c447f2b1b24679306e9cc8d82aeb92be10ff
Galaxy-Wanderer <55321981+Galaxy-Wanderer@users.noreply.github.com>
2019-09-16 13:54:55 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-        var Pixel = function(x, y, clr, size) {
-            var pixel = document.createElement("div");
-            pixel.style.backgroundColor = clr + "";
-            pixel.style.position = "absolute";
-            pixel.style.left = x + "px";
-            pixel.style.top = y + "px";
-            pixel.style.width = size + "px";
-            pixel.style.height = size + "px";
-            pixel.className = "pixelcolor";
-            document.body.appendChild(pixel);
-        };
-        Pixel(200, 200, "blue", 10);
+
